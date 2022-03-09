ec74b40aec1a28bd4091436529b6b419323f23d9
wujing <wujing50@huawei.com>
2022-03-09 18:17:15 +0800

    Increased flexibility
    Signed-off-by: wujing <wujing50@huawei.com>

--- rubikanalysis/analysis.py
+++ rubikanalysis/analysis.py
@@ -176,16 +176,19 @@ st.markdown("# ⎈ 混部干扰建模分析工具")
 st.markdown("## 业务介绍 TODO")
 st.markdown("## 环境说明")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file, index_col="Item")
-    st.write(data)
+node_info = pd.read_csv("../tests/data/default/node.csv", index_col="Item")
+uploaded_node_file = st.file_uploader("上传测试环境配置", type="csv")
+if uploaded_node_file is not None:
+    node_info = pd.read_csv(uploaded_node_file, index_col="Item")
 
-node_info = pd.read_csv("../tests/data/node.csv", index_col="Item")
 st.table(node_info)
 
 st.markdown("## 指标数据")
-data = pd.read_csv("../tests/data/clickhouse/l3cache_stress.csv")
+data = pd.read_csv("../tests/data/default/nginx.csv")
+uploaded_metrics_file = st.file_uploader("上传指标数据-QoS数据", type="csv")
+if uploaded_metrics_file is not None:
+    data = pd.read_csv(uploaded_metrics_file)
+
 mode = st.radio(
     "Please select a mode to visualize the data:",
     ('origin', 'normalization', 'standardization'))
@@ -208,7 +211,11 @@ st.altair_chart(chart, use_container_width=True)
 st.markdown("## 资源敏感度分析")
 
 # type stress avg-qos degradation-percent
-stress = pd.read_csv("../tests/data/stress.csv", keep_default_na=False)
+stress = pd.read_csv("../tests/data/default/stress.csv", keep_default_na=False)
+uploaded_stress_file = st.file_uploader("上传压力测试指标数据", type="csv")
+if uploaded_stress_file is not None:
+    stress = pd.read_csv(uploaded_stress_file)
+
 stress_all_symbols = ["cpu", "memory", "disk io", "cache", "network"]
 stress_unique_symbols = stress.type.unique()
 
@@ -255,17 +262,24 @@ st.markdown("### 热力图")
 # spearman相关系数: 非线性的、非正态数据
 # Kendall相关系数:  分类变量、无序数据
 fig, ax = plt.subplots()
-metrics_correlation = data.corr(method="pearson")
+corr_mode = st.radio(
+    """选择相关性分析方法:
+    pearson: 连续、正态分布、线性数据;
+    spearman: 非线性的、非正态数据;
+    kendall: 分类变量、无序数据""",
+    ('pearson', 'spearman', 'Kendall'))
+
+if corr_mode == 'spearman':
+    metrics_correlation = data.corr(method="spearman")
+elif corr_mode == 'Kendall':
+    metrics_correlation = data.corr(method="kendall")
+else:
+    metrics_correlation = data.corr(method="pearson")
+
 sns.heatmap(metrics_correlation, ax=ax)
 st.write(fig)
 
 # fig = sns.pairplot(data)
-# st.pyplot(fig)
-
-# fig = sns.jointplot(x='branch_misses', y='qos', data=data, kind='reg')
-# st.pyplot(fig)
-
-# fig = sns.jointplot(x='topdown_bad_spec', y='qos', data=data, kind='reg')
 # st.pyplot(fig)
 
 st.info("|r|>0.95存在显著性相关;|r|≥0.8高度相关;0.5≤|r|<0.8 中度相关;0.3≤|r|<0.5低度相关;|r|<0.3关系极弱")
@@ -273,15 +287,34 @@ st.markdown("### 相关性指标排序")
 metrics_correlation.iloc[-1] = abs(metrics_correlation.iloc[-1])
 st.table(metrics_correlation.iloc[-1].sort_values(ascending=False))
 
+metrics_correlation.iloc[-1] = metrics_correlation.iloc[-1].sort_values(
+    ascending=False)
 
-vaild_metrics = metrics_correlation[metrics_correlation["qos"] > 0.2].index.tolist(
+vaild_metrics = metrics_correlation[abs(metrics_correlation["qos"]) > 0.2].index.tolist(
 )
 vaild_metrics.remove("qos")
 st.markdown("#### 筛选有效指标")
+
 col = st.columns(len(vaild_metrics))
+label_col = 5
+label_raw = len(vaild_metrics) // label_col + 1
+
+for raw in range(label_raw):
+    show_label_count = label_col if len(
+        vaild_metrics) // label_col > 0 else len(vaild_metrics) % label_col
+    stcolumns = st.columns(show_label_count)
+    for col in range(show_label_count):
+        index = raw * label_col + col
+        index
+        corr_value = "{:.5}".format(
+            metrics_correlation.iloc[-1][vaild_metrics[index]])
+        stcolumns[col].metric(vaild_metrics[index], corr_value, index + 1)
+
 for i in range(len(vaild_metrics)):
-    corr_value = "{:.5}".format(metrics_correlation.iloc[-1][vaild_metrics[i]])
-    col[i].metric(vaild_metrics[i], corr_value, i + 1)
+    if i >= 4:
+        break
+    fig = sns.jointplot(x=vaild_metrics[i], y='qos', data=data, kind='reg')
+    st.pyplot(fig)
 
 st.markdown("### 回归拟合分析")
 # 数据预处理: 去除无效值; 特性缩放:标准化; 模型训练
